85bc3196a59ddde6cbe65e76964340014d7c6b14
Denys Parkhomenko <nokorimashita@gmail.com>
2020-11-17 15:08:01 +0200

    updated Newsletter sign up form

--- test_tasks/cartridge/controllers/NewsletterForm.js
+++ test_tasks/cartridge/controllers/NewsletterForm.js
@@ -10,35 +10,39 @@ var app = require('*/cartridge/scripts/app');
 var guard = require('storefront_controllers/cartridge/scripts/guard');
 var URLUtils = require('dw/web/URLUtils');
 
-exports.Start = guard.ensure(['get'], function () {
+function Start() {
     app.getView({
         ContinueURL: URLUtils.https('NewsletterForm-HandleForm'),
         CurrentForms: session.forms
     }).render('newsletter/newslettersignup');
-});
+}
 
-exports.HandleForm = guard.ensure(['post'], function () {
+function HandleForm() {
     var currentForm = app.getForm('newsletter');
 
     currentForm.handleAction({
         subscribe: function () {
-            var resultTemplate = 'newslettersuccess';
             try {
                 dw.system.Transaction.wrap(function () {
-                    require('~/cartridge/scripts/customobjects/NewsletterSubscription').createObject(currentForm);
+                    require('~/cartridge/scripts/customobjects/NewsletterSubscription').createObjectByForm(currentForm);
                 });
+                app.getView({
+                    CurrentForms: session.forms
+                }).render('newsletter/newslettersuccess');
+    
+                currentForm.clearFormElement(); 
             } catch (error) {
-                resultTemplate = 'newslettererror';
+            	currentForm.get('email').invalidateFormElement();
+                response.redirect(URLUtils.https('NewsletterForm-Start'));
             }
-
-            app.getView({
-                CurrentForms: session.forms
-            }).render('newsletter/' + resultTemplate);
-
-            currentForm.clearFormElement(); 
         },
         error: function () {
             response.redirect(URLUtils.https('NewsletterForm-Start'));
         }
     });
-});
+}
+
+
+/* Web exposed methods */
+exports.Start = guard.ensure(['get'], Start);
+exports.HandleForm = guard.ensure(['post'], HandleForm);
